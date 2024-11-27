6508eb9c173c0eb42e02360f94f06925ee3dc46d
Santosh Pujari <santosh.pujari@hotmail.com>
2024-11-27 12:28:09 -0600

    Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import logo from '../../assets/logo.png';
 
 const Login = () => {
+  const [signState, setSignState] = useState('Sign In');
+
   return (
     <div className='login'>
       <img src={logo} alt='' className='login-logo' />
       <div className='login-form'>
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type='text' name='' id='' placeholder='Your name' />
-          <input type='email' name='' id='' placeholder='email' />
-          <input type='password' name='' id='' placeholder='password' />
-          <button>Sign Up</button>
+          {signState === 'Sign Up' ? (
+            <input type='text' name='' id='' placeholder='Your name' />
+          ) : (
+            <></>
+          )}
+          <input type='email' name='' id='' placeholder='Email' />
+          <input type='password' name='' id='' placeholder='Password' />
+          <button>{signState}</button>
           <div className='form-help'>
             <div className='remember'>
               <input type='checkbox' name='' id='' />
@@ -21,6 +27,31 @@ const Login = () => {
             <p>Need Help?</p>
           </div>
         </form>
+        <div className='form-switch'>
+          {signState === 'Sign In' ? (
+            <p>
+              New to Netflix?{' '}
+              <span
+                onClick={() => {
+                  setSignState('Sign Up');
+                }}
+              >
+                Sign Up Now
+              </span>
+            </p>
+          ) : (
+            <p>
+              Already have account?{' '}
+              <span
+                onClick={() => {
+                  setSignState('Sign In');
+                }}
+              >
+                Sign In Now
+              </span>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
