1995516c8d84c2fb6367ec850e7fe5460a5dba8c
Santosh Pujari <santosh.pujari@hotmail.com>
2024-11-27 14:34:50 -0600

    Fetch data from tmdb API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data';
 
@@ -10,19 +10,43 @@ const TitleCards = ({ title, category }) => {
     cardsRef.current.scrollleft += event.deltaY;
   };
 
+  const [apiData, setApiData] = useState([]);
+
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : 'now_playing'
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel);
   }, []);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDM5NTJkYzkyNzQyYmU3ZTU5MDM4ZjU5MGI3NmM3ZSIsIm5iZiI6MTczMjczNjMyNy40NTYxODg3LCJzdWIiOiI2NzQ3NjU4OWRhNzY3YWFlMzA0MGQyMDkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.VthwHwRKa2LY6v-dXmd-H2snqs9dmCUL-mmKWyAfIjM',
+    },
+  };
 
   return (
     <div className='title-cards'>
       <h2>{title ? title : 'Popular on Netflix'}</h2>
       <div className='card-list' ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className='card' key={index}>
-              <img src={card.image} alt='' />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.poster_path}
+                alt=''
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
