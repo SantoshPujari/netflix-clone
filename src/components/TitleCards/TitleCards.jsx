3650c0ef0a25d67bf7512d44ccf96d3da4921086
Santosh Pujari <santosh.pujari@hotmail.com>
2025-08-05 17:15:56 -0500

    first commit

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './TitleCards.css';
+
+const TitleCards = () => {
+  return <div className='title-cards'>Title Cards Component</div>;
+};
+
+export default TitleCards;
