acd72f8e6b6c7192e488a9c9414da4a3ecd6ad3b
Santosh Pujari <santosh.pujari@hotmail.com>
2025-08-06 16:23:58 -0500

    Updated TitleCard functionality

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './TitleCards.css';
+import cards_data from '../../assets/cards/Cards_data';
 
 const TitleCards = () => {
-  return <div className='title-cards'>Title Cards Component</div>;
+  const cardsRef = React.useRef(null);
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  }, []);
+
+  return (
+    <div className='title-cards'>
+      <h2>Popular on Netflex</h2>
+      <div className='card-list' ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className='card' key={index}>
+              <img src={card.image} alt='' />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
